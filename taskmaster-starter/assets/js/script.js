0dadde2ddfc700b9835ef05c518aa20c6937e9d0
Matthew O <mo@pop-os.localdomain>
2020-08-24 20:37:40 -0700

    used jquery to add drag/drop elements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -140,6 +140,72 @@ $(".list-group").on("blur", "input[type='text']", function(){
   $(this).replaceWith(taskSpan);
 })
 
+// turns every element with list-group to link with other items in same class
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+    console.log("activate", this);
+  },
+  deactivate: function(event){
+    console.log("deactivate", this);
+  },
+  over: function(event){
+    console.log("over", this);
+  },
+  out: function(event){
+    console.log("out", this);
+  },
+
+  // jQuery to get all children in an array
+  update: function(){
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function(){
+        // save values in temp array
+        tempArr.push({
+          text: $(this)
+            .find("p")
+            .text()
+            .trim(),
+          date: $(this)
+            .find("span")
+            .text()
+            .trim()
+        });
+      });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+
+// creates droppable trash section
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui){
+    ui.draggable.remove();
+  },
+  over: function(event, ui){
+    console.log("over");
+  },
+  out: function(event, ui){
+    console.log("out");
+  }
+});
+
 //-------MODAL START-------
 
 // modal was triggered
@@ -187,5 +253,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
