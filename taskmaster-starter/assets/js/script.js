e3a5d0a6ce215050f09def4dfed478a3a3383388
Matthew O <mo@pop-os.localdomain>
2020-08-29 15:59:28 -0700

    finished touch-ups

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -171,16 +171,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event){
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event){
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event){
-    console.log("over", this);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event){
-    console.log("out", this);
+    $(event.target).removeClass("dropover-active");
   },
 
   // jQuery to get all children in an array
@@ -221,12 +223,15 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui){
     ui.draggable.remove();
+    $("bottom-trash").removeClass(".bottom-trash-active");
   },
   over: function(event, ui){
     console.log("over");
+    $("bottom-trash").addClass(".bottom-trash-active");
   },
   out: function(event, ui){
     console.log("out");
+    $("bottom-trash").removeClass(".bottom-trash-active");
   }
 });
 
@@ -245,7 +250,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
